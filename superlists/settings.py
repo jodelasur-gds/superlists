706c74cbb51a1b9f1666dcfc5843f2ec31b68d49
jodelasur-gds <jodel.asur@gdslink.com>
2019-06-25 16:28:52 +0800

    Use XMLTestRunner for tests

--- superlists/settings.py
+++ superlists/settings.py
@@ -113,3 +113,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Test Runner settings to enable xUnit XML outputs, for Bamboo integration
+TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
+TEST_OUTPUT_VERBOSE = True
+TEST_OUTPUT_DESCRIPTIONS = True
+TEST_OUTPUT_DIR = 'test-reports'
